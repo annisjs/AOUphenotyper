b279d361a85182ab70f61fc0c49ec2cf9670dd43
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2023-01-10 11:08:28 -0600

    fitbit cleaning algo, read_bucket now assumes bucket address

--- R/utilities.R
+++ R/utilities.R
@@ -8,15 +8,17 @@
 #' @export
 read_bucket <- function(export_path) {
   col_types <- NULL
-  as.data.table(dplyr::bind_rows(
+  bucket <- Sys.getenv("WORKSPACE_BUCKET")
+  export_path <- stringr::str_glue("{bucket}/{export_path}")
+  data.table::as.data.table(dplyr::bind_rows(
     purrr::map(system2('gsutil', args = c('ls', export_path), stdout = TRUE, stderr = TRUE),
-        function(csv) {
-          chunk <- readr::read_csv(pipe(str_glue('gsutil cat {csv}')), col_types = col_types, show_col_types = FALSE)
-          if (is.null(col_types)) {
-            col_types <- readr::spec(chunk)
-          }
-          chunk
-        })))
+               function(csv) {
+                 chunk <- readr::read_csv(pipe(str_glue('gsutil cat {csv}')), col_types = col_types, show_col_types = FALSE)
+                 if (is.null(col_types)) {
+                   col_types <- readr::spec(chunk)
+                 }
+                 chunk
+               })))
 }
 
 #' Download data given query
@@ -92,5 +94,38 @@ recode_sex_fm <- function(sex)
          ifelse(sex == "Male",
                 "Male",
                 "Female"))
+}
+
+#' Cleans fitbit data
+#' @export
+clean_fitbit <- function(fitbit_dat,wear_time,date_of_birth)
+{
+  fitbit_dat <- merge(fitbit_dat,wear_time,by=c("person_id","date"))
+  cat("\nInitial cohort")
+  cat("\nN: ",length(unique(fitbit_dat$person_id)))
+  cat("\nDays: ",nrow(fitbit_dat))
+
+  cat("\n\nRemoving days where wear time < 10 hrs.")
+  fitbit_dat <- fitbit_dat[wear_time >= 10]
+  cat("\nN: ",length(unique(fitbit_dat$person_id)))
+  cat("\nDays: ",nrow(fitbit_dat))
+
+  cat("\n\nRemoving days where step count < 100.")
+  fitbit_dat <- fitbit_dat[steps >= 100]
+  cat("\nN: ",length(unique(fitbit_dat$person_id)))
+  cat("\nDays: ",nrow(fitbit_dat))
+
+  cat("\n\nRemoving days where step counts > 45,000.")
+  fitbit_dat <- fitbit_dat[steps <= 45000]
+  cat("\nN: ",length(unique(fitbit_dat$person_id)))
+  cat("\nDays: ",nrow(fitbit_dat))
+
+  cat("\n\nRemoving days where age < 18.")
+  fitbit_dat <- merge(fitbit_dat,date_of_birth,by="person_id",all.x=TRUE)
+  fitbit_dat[,age := as.numeric(as.Date(date) - as.Date(date_of_birth))/365.25]
+  fitbit_dat <- fitbit_dat[age >= 18]
+  cat("\nN: ",length(unique(fitbit_dat$person_id)))
+  cat("\nDays: ",nrow(fitbit_dat))
+  fitbit_dat
 }
 
