5e87e658a9dd186cb868261c0f8d6f14463ec899
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2023-08-28 14:13:32 -0500

    fixed column mismatch for rbind

--- R/obesity_bmi.R
+++ R/obesity_bmi.R
@@ -193,6 +193,7 @@ obesity_bmi <- function(dataset,output_folder,anchor_date_table=NULL,before=NULL
 
   result_all <- merge(result_height,result_weight,by=c("person_id","measurement_date"))
   result_all[, bmi := weight / (height/100)^2]
+  result_all <- result_all[,c("person_id","measurement_date","bmi")]
   result_all <- rbind(result_all,result_bmi)
   result_all <- result_all[order(measurement_date)]
   result_all <- result_all[bmi >= 30]
