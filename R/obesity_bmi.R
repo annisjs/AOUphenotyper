1c22128d24f97ea9d81a38ce397962a3820eb3f2
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2023-08-28 14:59:56 -0500

    merge on person_id and date

--- R/obesity_bmi.R
+++ R/obesity_bmi.R
@@ -191,11 +191,8 @@ obesity_bmi <- function(dataset,output_folder,anchor_date_table=NULL,before=NULL
     result_weight <- result_weight[measurement_date <= max_window_date]
   }
 
-  result_height <- result_height[order(height,decreasing=T)]
-  result_height <- result_height[,.(max_height = height[1]),.(person_id)]
-
-  result_all <- merge(result_weight,result_height,by=c("person_id"))
-  result_all[, bmi := weight / (max_height/100)^2]
+  result_all <- merge(result_weight,result_height,by=c("person_id","measurement_date"))
+  result_all[, bmi := weight / (height/100)^2]
   result_all <- result_all[,c("person_id","measurement_date","bmi")]
   result_all <- rbind(result_all,result_bmi)
   result_all <- result_all[order(measurement_date)]
