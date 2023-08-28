c3dfb2c012688575b5f442a377cab90b3f1c6e7d
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2023-08-28 13:43:55 -0500

    Obesity BMI now includes weight/height

--- R/obesity_bmi.R
+++ R/obesity_bmi.R
@@ -13,7 +13,7 @@ obesity_bmi <- function(dataset,output_folder,anchor_date_table=NULL,before=NULL
   query <- str_glue("
         SELECT
             measurement.person_id,
-            measurement.measurement_date AS bmi_date,
+            measurement.measurement_date,
             measurement.value_as_number AS bmi
         FROM
             ( SELECT
@@ -55,20 +55,152 @@ obesity_bmi <- function(dataset,output_folder,anchor_date_table=NULL,before=NULL
                     )
                 ) measurement")
 
-  result_all <- as.data.table(download_data(query))
+  result_bmi <- as.data.table(download_data(query))
   if (!is.null(anchor_date_table))
   {
-    result_all <- as.data.table(merge(result_all,anchor_date_table,by="person_id"))
-    result_all[,min_window_date := anchor_date + before]
-    result_all[,max_window_date := anchor_date + after]
-    result_all <- result_all[bmi_date >= min_window_date]
-    result_all <- result_all[bmi_date <= max_window_date]
+    result_bmi <- as.data.table(merge(result_bmi,anchor_date_table,by="person_id"))
+    result_bmi[,min_window_date := anchor_date + before]
+    result_bmi[,max_window_date := anchor_date + after]
+    result_bmi <- result_bmi[bmi_date >= min_window_date]
+    result_bmi <- result_bmi[bmi_date <= max_window_date]
   }
+
+  # Height
+  query <- str_glue("
+        SELECT
+            measurement.person_id,
+            measurement.measurement_date,
+            measurement.value_as_number AS height
+        FROM
+            ( SELECT
+                *
+            FROM
+                `{dataset}.measurement` measurement
+            WHERE
+                (
+                    measurement_concept_id IN  (
+                        SELECT
+                            DISTINCT c.concept_id
+                        FROM
+                            `{dataset}.cb_criteria` c
+                        JOIN
+                            (
+                                select
+                                    cast(cr.id as string) as id
+                                FROM
+                                    `{dataset}.cb_criteria` cr
+                                WHERE
+                                    concept_id IN (
+                                        3036277, 3023540
+                                    )
+                                    AND full_text LIKE '%_rank1]%'
+                            ) a
+                                ON (
+                                    c.path LIKE CONCAT('%.',
+                                a.id,
+                                '.%')
+                                OR c.path LIKE CONCAT('%.',
+                                a.id)
+                                OR c.path LIKE CONCAT(a.id,
+                                '.%')
+                                OR c.path = a.id)
+                            WHERE
+                                is_standard = 1
+                                AND is_selectable = 1
+                            )
+                    )
+                ) measurement
+            LEFT JOIN
+                `{dataset}.concept` m_unit
+                    ON measurement.unit_concept_id = m_unit.concept_id")
+
+  bq_table_save(
+    bq_dataset_query(dataset, query, billing = Sys.getenv("GOOGLE_PROJECT")),
+    paste0(output_folder,"/aou_phenotyper/height_*.csv"),
+    destination_format = "CSV")
+  result_height <- as.data.table(read_bucket(str_glue("{output_folder}/aou_phenotyper/height_*.csv")))
+  if (!is.null(anchor_date_table))
+  {
+    result_height <- as.data.table(merge(result_height,anchor_date_table,by="person_id"))
+    result_height[,min_window_date := anchor_date + before]
+    result_height[,max_window_date := anchor_date + after]
+    result_height <- result_height[measurement_date >= min_window_date]
+    result_height <- result_height[measurement_date <= max_window_date]
+  }
+
+  # Weight
+  query <- str_glue("
+        SELECT
+            measurement.person_id,
+            measurement.measurement_date,
+            measurement.value_as_number AS weight
+        FROM
+            ( SELECT
+                *
+            FROM
+                `{dataset}.measurement` measurement
+            WHERE
+                (
+                    measurement_concept_id IN  (
+                        SELECT
+                            DISTINCT c.concept_id
+                        FROM
+                            `{dataset}.cb_criteria` c
+                        JOIN
+                            (
+                                select
+                                    cast(cr.id as string) as id
+                                FROM
+                                    `{dataset}.cb_criteria` cr
+                                WHERE
+                                    concept_id IN (
+                                        3025315, 3013762
+                                    )
+                                    AND full_text LIKE '%_rank1]%'
+                            ) a
+                                ON (
+                                    c.path LIKE CONCAT('%.',
+                                a.id,
+                                '.%')
+                                OR c.path LIKE CONCAT('%.',
+                                a.id)
+                                OR c.path LIKE CONCAT(a.id,
+                                '.%')
+                                OR c.path = a.id)
+                            WHERE
+                                is_standard = 1
+                                AND is_selectable = 1
+                            )
+                    )
+                ) measurement
+                LEFT JOIN
+                `{dataset}.concept` m_unit
+                    ON measurement.unit_concept_id = m_unit.concept_id")
+
+  bq_table_save(
+    bq_dataset_query(dataset, query, billing = Sys.getenv("GOOGLE_PROJECT")),
+    paste0(output_folder,"/aou_phenotyper/weight_*.csv"),
+    destination_format = "CSV")
+  result_weight <- as.data.table(read_bucket(str_glue("{output_folder}/aou_phenotyper/weight_*.csv")))
+  if (!is.null(anchor_date_table))
+  {
+    result_weight <- as.data.table(merge(result_weight,anchor_date_table,by="person_id"))
+    result_weight[,min_window_date := anchor_date + before]
+    result_weight[,max_window_date := anchor_date + after]
+    result_weight <- result_weight[measurement_date >= min_window_date]
+    result_weight <- result_weight[measurement_date <= max_window_date]
+  }
+
+  result_all <- merge(result_height,result_weight,by=c("person_id","measurement_date"))
+  result_all[, bmi := weight / (height/100)^2]
+  result_all <- result_all[,c("person_id","measurement_date","bmi")]
+  result_all <- rbind(result_all,result_bmi)
   result_all <- result_all[order(bmi_date)]
   result_all <- result_all[bmi >= 30]
   result_all <- result_all[,.(obesity_bmi_entry_date = bmi_date[1],
                               obesity_bmi_value = bmi[1]),
                            .(person_id)]
+
   fwrite(result_all,file="obesity_bmi.csv")
   system(str_glue("gsutil cp obesity_bmi.csv {output_folder}/obesity_bmi.csv"),intern=TRUE)
 }
