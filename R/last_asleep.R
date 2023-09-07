1aec4c06a47b3ddb0ca7eb6d5b0c12bb06df8985
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2023-09-07 09:14:44 -0500

    first/last asleep gets all sleep level variables for better coverage

--- R/last_asleep.R
+++ R/last_asleep.R
@@ -19,7 +19,7 @@ last_asleep <- function(dataset,output_folder,anchor_date_table=NULL,before=NULL
         FROM (SELECT person_id, sleep_date, start_datetime, duration_in_min, is_main_sleep,
                row_number() over(partition by person_id, sleep_date order by start_datetime desc) as rn
                 FROM sleep_level
-                WHERE level = 'asleep' AND is_main_sleep = 'true') as t1
+                WHERE is_main_sleep = 'true') as t1
         WHERE rn = 1",sep="")
   bq_table_save(
     bq_dataset_query(dataset, query, billing = Sys.getenv("GOOGLE_PROJECT")),
