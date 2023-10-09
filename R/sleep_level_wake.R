72b927eb8d7e35ce7906722667a7605a6b2ec440
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2023-10-09 13:16:34 -0500

    Wake sleep level

---
+++ R/sleep_level_wake.R
@@ -0,0 +1,49 @@
+#' Wake sleep level data from Fitbit
+#' @param dataset a string returned by Sys.getenv("WORKSPACE_CDR"). Can also use another dataset, but this is not recommended.
+#' @param output_folder the folder to write the output to. Must be preceded by the workspace bucket location given by Sys.getenv("WORKSPACE_BUCKET").
+#' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
+#' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
+#' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
+#' @return output_folder/sleep_level_wake.csv
+#' @details Fitbit sleep data contains:
+#' 	person_id
+#' 	sleep_date
+#' 	start_datetime
+#' 	is_main_sleep
+#' 	duration_in_min
+#' @import stringr bigrquery
+#' @export
+sleep_level_wake <- function(dataset,output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
+{
+  query <- paste("
+        SELECT
+          person_id
+          , sleep_date AS sleep_level_wake_sleep_date
+          , start_datetime AS sleep_level_wake_start_datetime
+          , is_main_sleep AS sleep_level_wake_is_main_sleep
+          , duration_in_min AS sleep_level_wake_duration_in_min
+        FROM
+            `sleep_level` sleep_level
+        WHERE level = 'wake'", sep="")
+  bq_table_save(
+    bq_dataset_query(dataset, query, billing = Sys.getenv("GOOGLE_PROJECT")),
+    paste0(output_folder,"/aou_phenotyper/sleep_level_wake_*.csv"),
+    destination_format = "CSV")
+  result <- read_bucket(paste0(output_folder,"/aou_phenotyper/sleep_level_wake_*.csv"))
+  if (!is.null(anchor_date_table))
+  {
+    result <- as.data.table(merge(result,anchor_date_table,by="person_id"))
+    result[,min_window_date := anchor_date + before]
+    result[,max_window_date := anchor_date + after]
+    result <- result[sleep_level_wake_sleep_date >= min_window_date]
+    result <- result[sleep_level_wake_sleep_date <= max_window_date]
+    result <- result[,c('person_id',
+                        'sleep_level_wake_sleep_date',
+                        'sleep_level_wake_start_datetime',
+                        'sleep_level_wake_is_main_sleep',
+                        'sleep_level_wake_duration_in_min')]
+  }
+  fwrite(result,file="sleep_level_wake.csv")
+  system(str_glue("gsutil cp sleep_level_wake.csv {output_folder}/sleep_level_wake.csv"),intern=TRUE)
+  system(str_glue("gsutil rm {output_folder}/aou_phenotyper/*"),intern=TRUE)
+}
