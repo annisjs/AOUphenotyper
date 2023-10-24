dc1abd58a450f6eb47ba0e71548776976ce4f54e
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2023-10-24 12:21:42 -0500

    No NA's allowed in monthly observation check

--- R/format_fitbit_sleep_and_activity_cox.R
+++ R/format_fitbit_sleep_and_activity_cox.R
@@ -89,37 +89,37 @@ format_fitbit_sleep_and_activity_cox <- function(sleep_pa,dx,last_medical_encoun
 
   if ("minute_asleep" %in% cull_list)
   {
-    merged_cox[,count := length(minute_asleep),.(person_id,time1,time2)]
+    merged_cox[,count := length(which(!is.na(minute_asleep))),.(person_id,time1,time2)]
     merged_cox <- merged_cox[count >= 15]
   }
 
   if ("minute_rem" %in% cull_list)
   {
-    merged_cox[,count := length(minute_rem),.(person_id,time1,time2)]
+    merged_cox[,count := length(which(!is.na(minute_rem))),.(person_id,time1,time2)]
     merged_cox <- merged_cox[count >= 15]
   }
 
   if ("minute_deep" %in% cull_list)
   {
-    merged_cox[,count := length(minute_deep),.(person_id,time1,time2)]
+    merged_cox[,count := length(which(!is.na(minute_deep))),.(person_id,time1,time2)]
     merged_cox <- merged_cox[count >= 15]
   }
 
   if ("minute_light" %in% cull_list)
   {
-    merged_cox[,count := length(minute_light),.(person_id,time1,time2)]
+    merged_cox[,count := length(which(!is.na(minute_light))),.(person_id,time1,time2)]
     merged_cox <- merged_cox[count >= 15]
   }
 
   if ("minute_restless" %in% cull_list)
   {
-    merged_cox[,count := length(minute_restless),.(person_id,time1,time2)]
+    merged_cox[,count := length(which(!is.na(minute_restless))),.(person_id,time1,time2)]
     merged_cox <- merged_cox[count >= 15]
   }
 
   if ("minute_awake" %in% cull_list)
   {
-    merged_cox[,count := length(minute_awake),.(person_id,time1,time2)]
+    merged_cox[,count := length(which(!is.na(minute_awake))),.(person_id,time1,time2)]
     merged_cox <- merged_cox[count >= 15]
   }
 
