b1d1e7438c5061edd11f4cb602aba678a3bb483c
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2023-11-03 13:21:45 -0500

    Closest BMI algorithm that doesn't use height/weight to compute BMI when it is missing

---
+++ R/closest_bmi2.R
@@ -0,0 +1,172 @@
+#' Closest BMI (Using only the BMI measure)
+#' @param dataset a string returned by Sys.getenv("WORKSPACE_CDR"). Can also use another dataset, but this is not recommended.
+#' @param output_folder the folder to write the output to. Must be preceded by the workspace bucket location given by Sys.getenv("WORKSPACE_BUCKET").
+#' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
+#' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
+#' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
+#' @export
+#' @return output_folder/closest_bmi.csv
+#' @import data.table stringr
+#' @details In closest_bmi algorithm, height and weight are used when BMI is missing. closest_bmi2 only uses the BMI measurement.
+closest_bmi2 <-  function(dataset,output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
+{
+
+  if (is.null(anchor_date_table))
+  {
+    stop("closest_bmi is not a primary variable and requires an anchor date table.")
+  }
+
+  # BMI
+  bmi_query <- str_glue("
+        SELECT
+            measurement.person_id,
+            measurement.measurement_date,
+            measurement.value_as_number AS bmi
+        FROM
+            ( SELECT
+                *
+            FROM
+                `{dataset}.measurement` measurement
+            WHERE
+                (
+                    measurement_concept_id IN  (
+                        SELECT
+                            DISTINCT c.concept_id
+                        FROM
+                            `{dataset}.cb_criteria` c
+                        JOIN
+                            (
+                                select
+                                    cast(cr.id as string) as id
+                                FROM
+                                    `{dataset}.cb_criteria` cr
+                                WHERE
+                                    concept_id IN (
+                                        3038553
+                                    )
+                                    AND full_text LIKE '%_rank1]%'
+                            ) a
+                                ON (
+                                    c.path LIKE CONCAT('%.',
+                                a.id,
+                                '.%')
+                                OR c.path LIKE CONCAT('%.',
+                                a.id)
+                                OR c.path LIKE CONCAT(a.id,
+                                '.%')
+                                OR c.path = a.id)
+                            WHERE
+                                is_standard = 1
+                                AND is_selectable = 1
+                            )
+                    )
+                ) measurement")
+
+
+  # Height
+  height_query <- str_glue("
+        SELECT
+            measurement.person_id,
+            measurement.measurement_date,
+            measurement.value_as_number AS height
+        FROM
+            ( SELECT
+                *
+            FROM
+                `{dataset}.measurement` measurement
+            WHERE
+                (
+                    measurement_concept_id IN  (
+                        SELECT
+                            DISTINCT c.concept_id
+                        FROM
+                            `{dataset}.cb_criteria` c
+                        JOIN
+                            (
+                                select
+                                    cast(cr.id as string) as id
+                                FROM
+                                    `{dataset}.cb_criteria` cr
+                                WHERE
+                                    concept_id IN (
+                                        3036277, 3023540
+                                    )
+                                    AND full_text LIKE '%_rank1]%'
+                            ) a
+                                ON (
+                                    c.path LIKE CONCAT('%.',
+                                a.id,
+                                '.%')
+                                OR c.path LIKE CONCAT('%.',
+                                a.id)
+                                OR c.path LIKE CONCAT(a.id,
+                                '.%')
+                                OR c.path = a.id)
+                            WHERE
+                                is_standard = 1
+                                AND is_selectable = 1
+                            )
+                    )
+                ) measurement")
+
+  # Weight
+  weight_query <- str_glue("
+        SELECT
+            measurement.person_id,
+            measurement.measurement_date,
+            measurement.value_as_number AS weight
+        FROM
+            ( SELECT
+                *
+            FROM
+                `{dataset}.measurement` measurement
+            WHERE
+                (
+                    measurement_concept_id IN  (
+                        SELECT
+                            DISTINCT c.concept_id
+                        FROM
+                            `{dataset}.cb_criteria` c
+                        JOIN
+                            (
+                                select
+                                    cast(cr.id as string) as id
+                                FROM
+                                    `{dataset}.cb_criteria` cr
+                                WHERE
+                                    concept_id IN (
+                                        3025315, 3013762
+                                    )
+                                    AND full_text LIKE '%_rank1]%'
+                            ) a
+                                ON (
+                                    c.path LIKE CONCAT('%.',
+                                a.id,
+                                '.%')
+                                OR c.path LIKE CONCAT('%.',
+                                a.id)
+                                OR c.path LIKE CONCAT(a.id,
+                                '.%')
+                                OR c.path = a.id)
+                            WHERE
+                                is_standard = 1
+                                AND is_selectable = 1
+                            )
+                    )
+                ) measurement")
+
+  # BMI
+  result_bmi <- download_data(bmi_query)
+  result_bmi <- as.data.table(merge(result_bmi,anchor_date_table,by="person_id",allow.cartesian=TRUE))
+  result_bmi[,min_window_date := anchor_date + before]
+  result_bmi[,max_window_date := anchor_date + after]
+  result_bmi <- result_bmi[measurement_date >= min_window_date]
+  result_bmi <- result_bmi[measurement_date <= max_window_date]
+  result_bmi[,diff := abs(as.numeric(as.Date(measurement_date) - as.Date(anchor_date)))]
+  result_bmi <- result_bmi[order(diff)]
+  result_bmi <- result_bmi[,.(closest_bmi_entry_date = measurement_date[1],
+                              closest_bmi_value = bmi[1]),.(person_id,anchor_date)]
+  fwrite(result_bmi,file="closest_bmi2.csv")
+  system(str_glue("gsutil cp closest_bmi2.csv {output_folder}/closest_bmi2.csv"),intern=TRUE)
+
+}
