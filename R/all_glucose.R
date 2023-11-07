87faf30f452c3d13e165cb0acaab9505809d9b89
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2023-11-07 11:31:11 -0600

    All glucose

---
+++ R/all_glucose.R
@@ -0,0 +1,31 @@
+#' All glucose
+#' @param dataset a string returned by Sys.getenv("WORKSPACE_CDR"). Can also use another dataset, but this is not recommended.
+#' @param output_folder the folder to write the output to. Must be preceded by the workspace bucket location given by Sys.getenv("WORKSPACE_BUCKET").
+#' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
+#' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
+#' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
+#' @details Searches for
+#'
+#' "Glucose \[Mass/volume] in Serum or Plasma"
+#'
+#' "Glucose \[Mass/volume] in Blood"
+#' @return output_folder/all_glucose.csv
+#' @import data.table stringr
+#' @export
+all_glucose <- function(dataset,output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
+{
+  result_all <- lab_query(dataset,c("Glucose [Mass/volume] in Serum or Plasma",
+                                    "Glucose [Mass/volume] in Blood"))
+  if (!is.null(anchor_date_table))
+  {
+    result_all <- as.data.table(merge(result_all,anchor_date_table,by="person_id"))
+    result_all[,min_window_date := anchor_date + before]
+    result_all[,max_window_date := anchor_date + after]
+    result_all <- result_all[measurement_date >= min_window_date]
+    result_all <- result_all[measurement_date <= max_window_date]
+  }
+  colnames(result_all) <- c("person_id","all_glucose_date","all_glucose_value")
+  fwrite(result_all,file="all_glucose.csv")
+  system(str_glue("gsutil cp all_glucose.csv {output_folder}/all_glucose.csv"),intern=TRUE)
+}
+
