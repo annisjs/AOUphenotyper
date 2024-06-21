73b5cde238fec4b4002a62a42041a78032cfb3c4
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-06-21 09:55:55 -0500

    add deprecation warning

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("This package has been deprecated and is no longer being developed. Please use aou.phenotyper2: https://github.com/annisjs/aou.phenotyper2")
+}
